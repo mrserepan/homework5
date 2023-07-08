1b76dcdf9cbe5c8d83b9c42bec09113e4c415719
Sergey <serepan3@yandex.ru>
2023-07-08 12:16:20 +0700

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,44 @@
+﻿﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+
+int[] FillArray(int size = 123, int beginRange = 0, int endRange = 1000)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(beginRange, endRange);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine(string.Join(",  ", array));
+}
+
+int SumTwoDigit(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] < 100)
+        {
+            System.Console.Write(array[i] + " ");
+            count += 1; 
+        }
+    }
+    return count;
+}
+
+//----------------------------------------------------------------------------
+
+int[] array = FillArray();
+PrintArray(array);
+System.Console.WriteLine();
+System.Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [10,99]: {SumTwoDigit(array)}");
