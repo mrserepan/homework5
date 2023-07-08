c7b0a1aaf3a354a21357d23beceea2f22a092bb6
Sergey <serepan3@yandex.ru>
2023-07-08 12:12:27 +0700

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,46 @@
+﻿
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] FillArray(int size, int beginRange = 100, int endRange = 1000)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(beginRange, endRange);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine(string.Join(", ", array));
+}
+
+int SumEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count += 1;
+    }
+    return count;
+}
+
+//--------------------------------------------------------------------------------------
+
+int size = ReadInt("Задайте размер массива: ");
+int[] array = FillArray(size);
+PrintArray(array);
+
+System.Console.WriteLine($"Количество чётных элементов в массиве: {SumEven(array)}");
